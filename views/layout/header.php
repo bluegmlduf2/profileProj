4df65bd0f63ab5842d260e2c4bf63283f418f9d7
keung <38097602+bluegmlduf2@users.noreply.github.com>
2020-05-28 23:18:38 +0900

    head update

--- views/layout/header.php
+++ views/layout/header.php
@@ -5,19 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">    
 	<meta name="viewport" content="width=device-width, initial-scale=1"><!--bootStrap-->
     <title><?php echo $title; ?></title>
-    <!--css-->
-	
+
 	<!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../application/libraries/plugin/bootstrap/css/bootstrap.min.css" ><!--bootStrap-->
     <link rel="stylesheet" href="../application/libraries/style/common.css">
-    <link rel="stylesheet" href="../application/libraries/plugin/datatables.min.css"><!--datatables-->
-    
-	<!-- <link rel="stylesheet" href="../application/libraries/style/nm_form.css">
-    <link rel="stylesheet" href="../application/libraries/style/nm_list.css">-->
     
 	<!--js-->
 	<script src="../application/libraries/plugin/jquery-3.5.0.min.js"></script><!--jQuery-->
-    <script src="../application/libraries/plugin/datatables.min.js"></script><!--datatables-->
 	<script src="../application/libraries/plugin/bootstrap/js/bootstrap.min.js"></script><!--bootStrap-->
 	<!-- <script src="../application/libraries/plugin/bootstrap/js/popper.min.js"></script> --><!--bootStrap-->
 
